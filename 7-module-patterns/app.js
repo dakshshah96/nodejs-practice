907019fe5987f6e6105681ab9a98ef8b01a1e1fb
Daksh Shah <dakshshah96@gmail.com>
2017-05-22 15:09:31 +0530

    Lesson 7 - Module Patterns

---
+++ 7-module-patterns/app.js
@@ -0,0 +1,6 @@
+const stuff = require('./stuff')
+ 
+console.log(stuff.counter(['Daksh', 'Shawn', 'Roy', 'Joy']))
+console.log(stuff.adder(5, 9))
+console.log(stuff.adder(stuff.pi, 5))
+console.log(stuff.pi)
---
+++ 7-module-patterns/stuff.js
@@ -0,0 +1,7 @@
+const counter = arr => `There are ${arr.length} elements in this array.`
+
+const adder = (a, b) => `The sum of ${a} and ${b} is ${a + b}`
+
+const pi = 3.142
+
+module.exports = { counter, adder, pi }
