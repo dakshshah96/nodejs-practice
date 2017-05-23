85f381415327e3b3c2a87bda85b26448d743ec40
Daksh Shah <dakshshah96@gmail.com>
2017-05-23 10:30:39 +0530

    Lesson 21 - The package.json file

---
+++ 21-package-json-file/app.js
@@ -0,0 +1,11 @@
+/*
+
+        package.json file
+
+        * Helps keep track of project dependencies
+        * Need to share only core code while collaborating
+        * Use -save flag when installing package to save it to package.json
+        * Dependencies stay listed even on uninstalling package
+        * Use npm install to install dependencies for a project
+
+*/
